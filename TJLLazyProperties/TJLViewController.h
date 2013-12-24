5bbda772af88393da0034c99dd5c333da01d0815
Terry Lewis <terrylewis@outlook.com>
2013-12-24 14:42:03 -0600

    Initial Commit

---
+++ TJLLazyProperties/TJLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJLViewController.h
+//  TJLLazyProperties
+//
+//  Created by Terry Lewis II on 12/24/13.
+//  Copyright (c) 2013 Terry Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLViewController : UIViewController
+
+@end
